8e346fc5ad2cbd01e1007318582b1a7d6a154da4
HiPeng <32693265+neopen@users.noreply.github.com>
2026-04-17 18:32:36 +0800

    【桌面通知】自动关闭

--- src/renderer/js/modules/notification.js
+++ src/renderer/js/modules/notification.js
@@ -9,7 +9,7 @@ const NotificationModule = (function () {
 
     async function init() {
         _logger.info('NotificationModule.init called');
-        
+
         if (isElectron()) {
             // 在 Electron 环境中，直接设为 true，因为主进程有权限
             // 不需要通过 IPC 请求权限
@@ -17,19 +17,19 @@ const NotificationModule = (function () {
             permissionGranted = true;
             return true;
         }
-        
+
         // 浏览器环境
         if (!('Notification' in window)) {
             _logger.warn('Notifications not supported');
             return false;
         }
-        
+
         if (Notification.permission === 'granted') {
             permissionGranted = true;
             _logger.info('Notification permission already granted');
             return true;
         }
-        
+
         if (Notification.permission !== 'denied') {
             _logger.info('Requesting notification permission');
             try {
@@ -44,31 +44,31 @@ const NotificationModule = (function () {
             _logger.warn('Notification permission denied');
             permissionGranted = false;
         }
-        
+
         return permissionGranted;
     }
 
     function initWithoutWait() {
         _logger.info('NotificationModule.initWithoutWait called');
-        
+
         if (isElectron()) {
             _logger.info('Electron environment, setting permissionGranted = true');
             permissionGranted = true;
             return;
         }
-        
+
         // 浏览器环境
         if (!('Notification' in window)) {
             _logger.warn('Notifications not supported');
             return;
         }
-        
+
         if (Notification.permission === 'granted') {
             permissionGranted = true;
             _logger.info('Notification permission already granted');
             return;
         }
-        
+
         if (Notification.permission !== 'denied') {
             _logger.info('Requesting notification permission (non-blocking)');
             Notification.requestPermission().then(permission => {
@@ -90,26 +90,26 @@ const NotificationModule = (function () {
         _logger.info('isEnabled:', isEnabled);
         _logger.info('permissionGranted:', permissionGranted);
         _logger.info('isElectron:', isElectron());
-        
+
         if (!isEnabled) {
             _logger.info('Notifications disabled in settings');
             return false;
         }
-        
+
         // 在 Electron 环境中，通过主进程发送通知
         if (isElectron()) {
             try {
                 const { ipcRenderer } = window.require('electron');
-                
+
                 const notificationOptions = {
                     title: title,
                     body: options.body || '',
                     silent: false,
                     requireInteraction: options.requireInteraction || false
                 };
-                
+
                 _logger.info('Sending to main process:', notificationOptions);
-                
+
                 // 使用异步方式发送，避免阻塞
                 ipcRenderer.send('show-notification-async', notificationOptions);
                 _logger.info('Notification request sent to main process');
@@ -119,18 +119,18 @@ const NotificationModule = (function () {
                 return false;
             }
         }
-        
+
         // 浏览器环境
         if (!('Notification' in window)) {
             _logger.warn('Notifications not supported');
             return false;
         }
-        
+
         if (Notification.permission !== 'granted') {
             _logger.warn('No notification permission');
             return false;
         }
-        
+
         try {
             const notification = new Notification(title, {
                 body: options.body || '',
@@ -138,14 +138,14 @@ const NotificationModule = (function () {
                 requireInteraction: options.requireInteraction || false,
                 tag: options.tag || 'health-reminder'
             });
-            
+
             notification.onclick = () => {
                 _logger.info('Notification clicked');
                 window.focus();
                 notification.close();
             };
-            
-            setTimeout(() => notification.close(), 10000);
+
+            setTimeout(() => notification.close(), 5000);
             _logger.info('Browser notification sent successfully');
             return true;
         } catch (e) {
